c6ef5118fe35f89141ecaeb72b6682d904ab321b
Codart-7 <iconmoa@gmail.com>
2023-01-23 16:35:47 +0100

    [fix] Q2

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,7 +38,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert (page > 0) and (page_size > 0)
+        assert type(page) == int and type(page_size) == int\
+          and (page > 0) and (page_size > 0)
         start, end = index_range(page, page_size)
         try:
             self.dataset()
